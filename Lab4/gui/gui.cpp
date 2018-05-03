1c8e4e3fa9f6d1bfe33ea513242d720105a41790
jc-labs <spam_box_jc@zoho.eu>
2018-05-03 19:01:04 +0300

    Lab4 project structure was defined.

---
+++ Lab4/gui/gui.cpp
@@ -0,0 +1,4 @@
+#include "gui.hpp"
+gui::gui(QWidget *parent) : QWidget(parent) {
+	ui.setupUi(this);
+}
---
+++ Lab4/gui/gui.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <QtWidgets/QWidget>
+#include "ui_gui.h"
+class gui : public QWidget{	
+	Q_OBJECT
+public:
+	gui(QWidget *parent = Q_NULLPTR);
+private:
+	Ui::guiClass ui;
+};
