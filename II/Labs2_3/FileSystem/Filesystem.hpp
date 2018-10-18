d605b14b1a10ad1bb5607b178b7c9bd8beef1767
jc-labs <spam_box_jc@zoho.eu>
2018-10-18 06:56:49 +0300

    Filesystem::mkdir() was implemented.

--- II/Labs2_3/FileSystem/Filesystem.hpp
+++ II/Labs2_3/FileSystem/Filesystem.hpp
@@ -2,9 +2,10 @@
 #include <string>
 #include <fstream>
 #include <bitset>
-#include <list>
+#include <vector>
 
 #define write_f(f, v) f.write(reinterpret_cast<const char *>(&v), sizeof(v));
+#define read_f(f, v) f.read(reinterpret_cast<char *>(&v), sizeof(v));
 
 enum class Filetype {
 	empty = 0,
@@ -13,13 +14,7 @@ enum class Filetype {
 };
 inline uint8_t operator*(Filetype t) { return uint8_t(t); }
 
-using Address = size_t;
-
-struct File {
-	Filetype type;
-	size_t size;
-	Address prev, next;
-};
+using Address = uint32_t;
 
 template <size_t block_size = 4096u, size_t block_number = 1024u>
 class Filesystem {
@@ -33,10 +28,43 @@ protected:
 		if (!m_storage)
 			throw std::exception("Unable to access filesystem. Make sure it was mounted.");
 	}
+	Address claim_free_block() {
+		state_check();
+
+		m_storage.seekg(1 + 4 + 4 + 1);
+		char buffer[7];
+		m_storage.read(buffer, 7);
+		if (std::string(buffer) != "Bitset")
+			throw std::exception("'/Bitset' file is corrupted. Unable to restore filesystem.");
+
+		Address size;
+		read_f(m_storage, size);
+		std::vector<Address> addrs(size);
+		for (Address i = 0; i < size; i++)
+			read_f(m_storage, addrs.at(i));
+
+		char b;
+		for (Address c = 0; c < addrs.size(); c++) {
+			m_storage.seekg(addrs.at(c) * block_size);
+			for (Address i = 0; i < block_size; i++) {
+				b = m_storage.peek();
+				if (b != 0xff)
+					for (char j = 0; j < 8; j++)
+						if (auto t = 1 << j; ~b & t) {
+							b |= t;
+							m_storage.seekp(m_storage.tellg());
+							m_storage.put(b);
+							return c * 8 + j;
+						}
+			}
+		}
+		throw std::exception("All of the blocks are occupied. It's impossible to allocate memory.");
+	}
 public:
 	explicit Filesystem() : m_current_directory(m_root_directory) {
 		static_assert(block_size > 16);
 		static_assert(block_number > 2);
+		m_storage.setstate(std::fstream::failbit);
 	}
 	void initialize(std::string const& filename) {
 		std::ofstream tf;
@@ -46,23 +74,22 @@ public:
 		
 		tf << *Filetype::dir; //root_dir type
 
-		uint32_t bff = 0;
-		write_f(tf, bff); //root_dir's '.'
+		Address bff = 0;
 		write_f(tf, bff); //root_dir's '..'
+		write_f(tf, bff); //root_dir's '.'
 
 		// file discriptor
-		tf << "Bitset\0"; //name
-		uint32_t bitset_size = block_number / block_size / 8 + 1;
+		tf << *Filetype::file << "Bitset" << '\0'; //name
+		Address bitset_size = block_number / block_size / 8 + 1;
 		write_f(tf, bitset_size); //size
-		for (uint32_t i = 1; i <= bitset_size; i++)
+		for (Address i = 1; i <= bitset_size; i++)
 			write_f(tf, i); //block_list
 
 		char buffer = char(0xff);
 		tf.seekp(block_size * 1);
 		while (bitset_size >= 8u)
 			tf.write(&buffer, 1);
-		for (uint32_t i = 0; i < 8u - bitset_size - 1u; i++)
-			buffer <<= 1;
+		for (buffer = 1; bitset_size; bitset_size--, buffer |= buffer << 1);
 		tf.write(&buffer, 1);
 			   		
 		tf.seekp(block_size * block_number - 1);
@@ -84,9 +111,51 @@ public:
 		throw std::exception("Unimplemented feature");
 	}
 	void create(std::string const& name) {
+		state_check();
 
 	}
 	void mkdir(std::string const& name) {
+		state_check();
+		
+		m_storage.seekg(m_current_directory * block_size + 9);
+		while (m_storage.get()) {
+			while (m_storage.get() != '\0');
+			Address size, temp;
+			read_f(m_storage, size);
+			while (size--)
+				read_f(m_storage, temp);
+		}
 
+		auto pos = Address(m_storage.tellg());
+		auto addr = claim_free_block();
+		m_storage.seekp(addr * block_size);
+		m_storage << *Filetype::dir; //dir type
+		write_f(m_storage, pos); //dir's '..'
+		write_f(m_storage, addr); //dir's '.'
+		m_storage << '\0';
+
+		m_storage.seekp(pos);
+		m_storage.seekp(-1, std::fstream::cur);
+		m_storage << *Filetype::dir << name << '\0';
+		write_f(m_storage, addr);
+	}
+	std::string pwd() {
+		state_check();
+
+		std::string ret = "";
+		Address temp = m_current_directory;
+		while (temp != 0) {
+			m_storage.seekg(temp * block_size + 1);
+			Address addr;
+			read_f(m_storage, addr);
+			m_storage.seekg(addr);
+			std::string tmp;
+			char b;
+			while (m_storage >> b)
+				tmp += b;
+			ret = tmp + '/' + ret;
+			temp = addr / block_size;
+		}
+		return ret.empty() ? "/" : ret;
 	}
 };
--- II/Labs2_3/FileSystem/ui_tool.cpp
+++ II/Labs2_3/FileSystem/ui_tool.cpp
@@ -5,36 +5,37 @@
 int process() {
 	Filesystem<> filesystem;
 	std::string string;
-	std::istringstream stream;
-	size_t size;
-
-	///temp
-	filesystem.initialize("f");
-	return 0;
 
 	std::cout << ": ";
 	while (std::getline(std::cin, string)) {
-		stream.str(string);
+		std::istringstream stream(string);
 		stream >> string;
 		if (string == "exit") {
 			return 0;
 		} else if (string == "initialize") {
+			if (!stream) throw std::exception("File name is expected.");
 			stream >> string;
 			filesystem.initialize(string);
 		} else if (string == "mount") {
+			if (!stream) throw std::exception("File name is expected.");
 			stream >> string;
 			filesystem.mount(string);
 		} else if (string == "unmount") {
 			filesystem.unmount();
 		} else if (string == "filestat") {
-			stream >> size;
-			filesystem.filestat(size);
+			//if (!stream) throw std::exception("File name is expected.");
+			//stream >> size;
+			//filesystem.filestat(size);
 		} else if (string == "create") {
+			if (!stream) throw std::exception("File name is expected.");
 			stream >> string;
 			filesystem.create(string);
 		} else if (string == "mkdir") {
+			if (!stream) throw std::exception("Folder name is expected.");
 			stream >> string;
-			filesystem.create(string);
+			filesystem.mkdir(string);
+		} else if (string == "pwd") {
+			std::cout << filesystem.pwd() << '\n';
 		} else
 			std::cout << "Unknown command.\n";
 		std::cout << ": ";
